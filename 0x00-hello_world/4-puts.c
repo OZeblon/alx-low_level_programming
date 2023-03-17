0e50821b2f4b88ebe26b33f30fb3131b0e186bdf
ozeblon <yomizeblon@gmail.com>
2023-03-16 19:30:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,14 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle,
- * This is followed by a new line."
- * reture 0.
+ * This is followed by a new line.
+ * Return: Always 0.
  */
 
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return(0);	
+
 }
