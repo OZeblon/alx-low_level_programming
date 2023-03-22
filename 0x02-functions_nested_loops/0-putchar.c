ca475c7cfe6b2b4ae27af3d6ffe3983b2d2d8ebf
ozeblon <yomizeblon@gmail.com>
2023-03-22 11:41:09 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 
+ *i/
+
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('w');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
